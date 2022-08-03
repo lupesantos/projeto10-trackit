4e5e27eda84f0b70df80f9d314b0dee1fd7a75f2
luciano <lpereira2306@gmail.com>
2022-08-03 07:28:57 -0300

    oi

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../assets/css/reset.css";
+import "../assets/css/style.css";
+
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+	return <div className="main">Oi</div>;
+}
+
+export default App;
